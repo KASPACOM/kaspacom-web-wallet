a48a9c0be4a9e1f91e090c092acb9c8049cca0b5
Gilad Mucharer <giladm11@gmail.com>
2025-04-01 23:50:28 -0500

    password

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -38,8 +38,8 @@ export class LoginComponent implements OnInit {
     });
 
     // for faster development
-    this.loginForm.get('password')?.setValue('123-asd');
-    this.onSubmit();
+    // this.loginForm.get('password')?.setValue('password');
+    // this.onSubmit();
   }
 
   get password() {

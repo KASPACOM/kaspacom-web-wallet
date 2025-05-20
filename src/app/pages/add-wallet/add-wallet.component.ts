ac4529ab7126a9b56968d307db01cfdae919bd6a
Gilad Mucharer <giladm11@gmail.com>
2025-05-19 21:11:46 -0500

    mnemonic

--- src/app/pages/add-wallet/add-wallet.component.ts
+++ src/app/pages/add-wallet/add-wallet.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { ImportWalletComponent } from '../../components/wallet-management/import-wallet/import-wallet.component';
 import { CreateWalletComponent } from '../../components/wallet-management/create-wallet/create-wallet.component';
+import { WalletService } from '../../services/wallet.service';
 
 @Component({
   selector: 'app-add-wallet',
@@ -16,4 +18,16 @@ import { CreateWalletComponent } from '../../components/wallet-management/create
 })
 export class AddWalletComponent {
   selectedAction: 'create' | 'import' = 'create';
+  hasExistingWallets = false;
+
+  constructor(
+    private walletService: WalletService,
+    private router: Router
+  ) {
+    this.hasExistingWallets = this.walletService.getWalletsCount() > 0;
+  }
+
+  goBack() {
+    this.router.navigate(['/wallet-selection']);
+  }
 }

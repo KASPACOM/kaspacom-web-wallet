2162a224d72fe3c41c73f8c3fe732aa1f51f47ad
Gilad Mucharer <giladm11@gmail.com>
2025-05-20 01:12:25 -0500

    text change

--- src/app/services/action-info-services/review-action-data.service.ts
+++ src/app/services/action-info-services/review-action-data.service.ts
@@ -344,7 +344,7 @@ export class ReviewActionDataService {
                 },
                 ...l2AdressRow,
                 {
-                    fieldName: "Don't ask again for this application and wallet",
+                    fieldName: "Don't ask again for this application & wallet",
                     fieldValue: "false",
                     inputField: {
                         fieldType: InputFieldType.CHECKBOX,

6a0210e1bb79075efaadef7b3efc3a1b14bb08ec
void <void.azos@gmail.com>
2025-05-26 01:05:49 +0200

    qf

--- src/app/v2/pages/onboarding-page/flows/new-wallet-flow/steps/error-new-wallet-step/error-new-wallet-step.component.ts
+++ src/app/v2/pages/onboarding-page/flows/new-wallet-flow/steps/error-new-wallet-step/error-new-wallet-step.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 
 @Component({
   selector: 'app-error-new-wallet-step',
   imports: [],
   templateUrl: './error-new-wallet-step.component.html',
-  styleUrl: './error-new-wallet-step.component.scss'
+  styleUrl: './error-new-wallet-step.component.scss',
 })
 export class ErrorNewWalletStepComponent {
-
+  next = output<void>();
+  previous = output<void>();
 }

7940ab6aa309a5a6c37427ab11135abedb51d983
JSmithOner <brigand.vincent@gmail.com>
2026-02-17 11:38:19 +0100

    added zero address

--- src/app/v2/pages/app/home/pending-transactions-banner/pending-transactions-banner.component.ts
+++ src/app/v2/pages/app/home/pending-transactions-banner/pending-transactions-banner.component.ts
@@ -1,12 +1,8 @@
-import { Component, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  KcIconComponent,
-  KcTooltipDirective,
-  KcSpinnerComponent,
-} from 'kaspacom-ui';
-import { WalletService } from '../../../../../services/wallet.service';
+import { Component, computed, inject, signal } from '@angular/core';
+import { KcIconComponent, KcSpinnerComponent } from 'kaspacom-ui';
 import { KaspaNetworkActionsService } from '../../../../../services/kaspa-netwrok-services/kaspa-network-actions.service';
+import { WalletService } from '../../../../../services/wallet.service';
 
 @Component({
   selector: 'app-pending-transactions-banner',

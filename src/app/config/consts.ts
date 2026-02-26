1d0bc281cddbcb54e5aa816102bfadfd750f1815
Gilad Mucharer <giladm11@gmail.com>
2026-02-26 13:34:50 -0300

    updating sdk

--- src/app/config/consts.ts
+++ src/app/config/consts.ts
@@ -11,6 +11,7 @@ export const LOCAL_STORAGE_KEYS = {
 export const KASPA_NETWORKS = {
   MAINNET: 'mainnet',
   TESTNET10: 'testnet-10',
+  TESTNET12: 'testnet-12',
 };
 
 export const DEFAULT_DERIVED_PATH = "m/44'/111111'/0'/0/0";

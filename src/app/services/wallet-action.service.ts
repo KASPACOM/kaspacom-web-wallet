4ffb49138d0e85bb7107e32754afea13df69c532
Gilad Mucharer <giladm11@gmail.com>
2026-06-09 09:04:53 -0500

    fix utxo without address

--- src/app/services/wallet-action.service.ts
+++ src/app/services/wallet-action.service.ts
@@ -895,9 +895,18 @@ export class WalletActionService {
     }
 
     for (const input of transaction.inputs) {
+      const utxoAddress = input.utxo.address || this.kaspaNetworkActionsService.getWalletAddressFromScriptPublicKey(input.utxo.scriptPublicKey);
+
+      if (!utxoAddress) {
+        return {
+          isValidated: false,
+          errorCode: ERROR_CODES.WALLET_ACTION.INVALID_PSKT_TX,
+        };
+      }
+
       const transactionInputWalletUtxos =
         await this.kaspaNetworkActionsService.getWalletBalanceAndUtxos(
-          input.utxo.address,
+          utxoAddress,
         );
 
       const transactionInputUtxo = transactionInputWalletUtxos.utxoEntries.find(

b579851852f8c30ca2cd0614c55cf155a32a704d
Gilad Mucharer <giladm11@gmail.com>
2026-06-09 14:15:13 -0500

    Revert "Merge pull request #230 from KASPACOM/add-pskt-compute-budget"
    This reverts commit d01305d00d0a6819fe4c2d0adda22b2a6e07d165, reversing
    changes made to 5f94c3756418e35ca8212fecfd807cfb4c336aed.

--- src/app/services/wallet-action.service.ts
+++ src/app/services/wallet-action.service.ts
@@ -219,7 +219,7 @@ export class WalletActionService {
 
     for (let input of transaction.inputs) {
       if (!('computeBudget' in input)) {
-        input.computeBudget = 0;
+        input.computeBudget = undefined;
       }
     }
 
